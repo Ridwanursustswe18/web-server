c4676ae18e0cccdea1e073e97796d18568762049
Ridwan <ridwanur196@gmail.com>
2025-03-23 12:21:40 +0600

    basic server created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException
+    {
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server is running on port 80");
+    }
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException
+        {
+            String response = "Requested path: " + exchange.getRequestURI().getPath();
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
